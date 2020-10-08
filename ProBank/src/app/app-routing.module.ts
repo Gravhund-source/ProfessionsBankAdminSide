7c65c9f6dbb269daa74f0e05493b1402a1cf3a44
Gravhund-source <victorgawron@yahoo.dk>
2020-10-08 10:29:06 +0200

    Opdatering: Login Restrektion

--- ProBank/src/app/app-routing.module.ts
+++ ProBank/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 import { AdminSideComponent } from './Admin/admin-side/admin-side.component';
 import { SignInComponent } from './Login/sign-in/sign-in.component';
-import { HttpService } from './Service/http.service';
+import { AuthGuard} from './auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch:'full' },
+  { path: '', redirectTo: 'login', pathMatch:'full'},
   { path: 'login', component: SignInComponent},
-  { path: 'admin', component: AdminSideComponent},
+  { path: 'admin', component: AdminSideComponent, canActivate:[
+    AuthGuard
+  ]},
 ];
 
 @NgModule({
